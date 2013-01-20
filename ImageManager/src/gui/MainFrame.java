2eca4dcda799539f47e807a3438525917966928a
bluk44 <bluk44@gmail.com>
2013-01-20 11:06:47 +0100

    zmiana L&F na GTK

--- ImageManager/src/gui/MainFrame.java
+++ ImageManager/src/gui/MainFrame.java
@@ -18,6 +18,8 @@ import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
 import javax.swing.LayoutStyle;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.WindowConstants;
 
 import component.ThumbnailComponent;
@@ -58,6 +60,14 @@ public class MainFrame extends javax.swing.JFrame {
 	 * Auto-generated main method to display this JFrame
 	 */
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				MainFrame inst = new MainFrame();
