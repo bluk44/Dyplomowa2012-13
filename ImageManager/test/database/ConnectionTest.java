e43f04a4e51ce715894831bdc4c1424754eff28a
bluk44 <bluk44@gmail.com>
2013-01-22 22:05:51 +0100

    test polaczenia z mysql ok

--- ImageManager/test/database/ConnectionTest.java
+++ ImageManager/test/database/ConnectionTest.java
@@ -2,6 +2,7 @@ package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class ConnectionTest {
@@ -18,21 +19,31 @@ public class ConnectionTest {
 	public static void main(String[] args) {
 		
 		Connection sqlConnection = null;
+		PreparedStatement preparedStatement = null;
 		try {
 			String connect = "jdbc:mysql://"+HOST+":"+PORT+"/"+DB_NAME;
 			System.out.println("connecting to: "+ connect);
 			sqlConnection = DriverManager.getConnection(connect, USER, PASS);
 			System.out.println("connection successful");
+			
+			addUser(sqlConnection, preparedStatement, "gruby");
+			addUser(sqlConnection, preparedStatement, "przecinek");
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
 			System.out.println("Closing sql connection...");
-			if(sqlConnection != null){
-				try { sqlConnection.close(); } catch (SQLException e) {}
-			}
+			if(sqlConnection != null) try { sqlConnection.close(); } catch (SQLException e) {}		
 			System.out.println("sql connection closed");
+			if(preparedStatement != null) try { preparedStatement.close(); } catch (SQLException e) {}		
+
 		}
 	}
-
+	
+	public static void addUser(Connection connection, PreparedStatement preparedStatement, String name) throws SQLException{
+		preparedStatement = connection.prepareStatement("INSERT INTO users (name) VALUES(?)");
+		preparedStatement.setString(1, name);
+		preparedStatement.executeUpdate();
+	}
 }
