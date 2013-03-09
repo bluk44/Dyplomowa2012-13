b7c4900dbdbd8790883e969a7f22b2c9b10d78ab
bluk44 <bluk44@gmail.com>
2013-03-09 06:04:11 +0100

    zmiana asocjacji z klasą rysującą histogram (GraphComp zamiast HistGraph)

--- Algorithms/src/algorithm/Histogram.java
+++ Algorithms/src/algorithm/Histogram.java
@@ -1,8 +1,5 @@
 package algorithm;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 import java.io.File;
@@ -10,19 +7,22 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 
 import org.jdesktop.swingx.JXGraph;
+
+import components.GraphComp;
 
 public class Histogram {
 	protected int nBands;
 	protected int[][] values;
-	protected HistGraph[] graphs;
+	protected GraphComp[] graphs;
 	
 	private int[] maxYs;
 	public Histogram(BufferedImage image) {
 		nBands = image.getRaster().getNumBands();
 		values = new int[nBands][256];
-		graphs = new HistGraph[nBands];
+		graphs = new GraphComp[nBands];
 		maxYs = new int[nBands];
 		
 		Raster r = image.getRaster();
@@ -37,9 +37,29 @@ public class Histogram {
 				}
 			}
 			maxYs[i] = maxY;
-			graphs[i] = new HistGraph(0, 0, 0, 255, 0, maxY, 32, 8, maxY / 10,
-					maxY / 15, i);
-			graphs[i].addPlots(Color.BLACK, new Values(i));
+			graphs[i] = new GraphComp(values[i]);
+		}
+	}
+	
+	public Histogram(BufferedImage image, int x, int y, int w, int h) {
+		nBands = image.getRaster().getNumBands();
+		values = new int[nBands][256];
+		graphs = new GraphComp[nBands];
+		maxYs = new int[nBands];
+		
+		Raster r = image.getRaster();
+		for (int i = 0; i < nBands; i++) {
+			int maxY = 0;
+			int[] samples = new int[w * h];
+			r.getSamples(x, y, w, h, i, samples);
+			for (int j = 0; j < samples.length; j++) {
+				values[i][samples[j]]++;
+				if (maxY < values[i][samples[j]]) {
+					maxY = values[i][samples[j]];
+				}
+			}
+			maxYs[i] = maxY;
+			graphs[i] = new GraphComp(values[i]);
 		}
 	}
 	
@@ -48,7 +68,7 @@ public class Histogram {
 	}
 	
 	public void setTreshold(int b, int val){
-		graphs[b].treshold = val;
+		graphs[b].setTreshold(val);
 		
 	}
 	
@@ -66,58 +86,24 @@ public class Histogram {
 		return ostr;
 	}
 
-	class Values extends JXGraph.Plot {
-		public Values(int band) {
-			super();
-			this.band = band;
-		}
-
-		int band;
-
-		@Override
-		public double compute(double value) {
-			if (value < 0.d || value > 255) {
-				return 0;
-			}
-			int val = (int) value;
-			return values[band][val];
-		}
-
-	}
-
-	class HistGraph extends JXGraph {
-		int b;
-		int treshold = -1;
-		public HistGraph(double originX, double originY, double minX,
-				double maxX, double minY, double maxY, double majorX,
-				int minorCountX, double majorY, int minorCountY, int b) {
-			super(originX, originY, minX, maxX, minY, maxY, majorX, minorCountX, majorY, minorCountY);
-			this.b = b;
-		}
-		
-		@Override
-		protected void paintExtra(Graphics2D g2) {
-			if(treshold < 0) return;
-			g2.setColor(Color.RED);
-			g2.drawLine((int)xPositionToPixel(treshold), 0, (int)xPositionToPixel(treshold), maxYs[b]);
-			
-		}
-	}
-
 	public void draw(int band) {
 		JFrame frame = new JFrame("plot of " + band);
-		frame.add(graphs[band]);
-		frame.setPreferredSize(graphs[band].getPreferredSize());
+		JScrollPane scrollPane = new JScrollPane(graphs[band]);
+		frame.add(scrollPane);
 		frame.pack();
 		frame.setVisible(true);
 	}
-
+	
+	public void draw(){
+		draw(0);
+	}
+	
 	public static void main(String[] args) {
 		try {
 			BufferedImage img = ImageIO.read(new File(
-					"images/half.bmp"));
+					"images/mars.bmp"));
 			Histogram h = new Histogram(img);
-			h.setTreshold(0, 255);
+			h.setTreshold(0, 210);
 			// System.out.println(h);
 			h.draw(0);
 		} catch (IOException e) {
