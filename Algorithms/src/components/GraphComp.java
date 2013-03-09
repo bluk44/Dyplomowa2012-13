f811a24a7ef9d8d558ad0843149f738a1785bccf
bluk44 <bluk44@gmail.com>
2013-03-09 05:54:19 +0100

    ostateczna wersja wykresu histogramu

--- Algorithms/src/components/GraphComp.java
+++ Algorithms/src/components/GraphComp.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
@@ -13,57 +15,63 @@ import javax.swing.JScrollPane;
 
 public class GraphComp extends JComponent {
 
-	static final int XAxisLenTotal = 544;
+	
+	static final int YMax = 250, XMax = 256;
 	static final int XZeroXPos = 60;
-	static final int XMaxXPos = 524;
+	static final int YZeroYPos = 20;
 	static final int pixPerUnit = 2;
-	private int XAxisX1, XAxisY, XAxisX2, XAxisY2;
+	
+	static final int XScaleLen = XMax * pixPerUnit;
+	static final int YScaleLen = YMax *pixPerUnit;
+	static final int XAxisLenTotal = XZeroXPos + XScaleLen + 5;
+	static final int YAxisLenTotal = YZeroYPos + YScaleLen + 5;
 
-	private int YAxisLenTotal;
-	private int YAxisX1, YAxisY1, YAxisX2, YAxisY2;
-	private double YAxisStep = 1, YMax = 250;
-
+	// x axis position
+	static final int XAxisX1 = 0, XAxisY = YAxisLenTotal - YZeroYPos, XAxisX2 = XAxisLenTotal;
+	
+	// y axis position
+	static final int YAxisX = 50, YAxisY1 = 0, YAxisY2 = YAxisLenTotal;
+	
+	private double YAxisStep = 1;
 	private int[] pixelCount;
-
-	GraphComp() {
-		YAxisLenTotal = 530;
-
-		// x axis position
-		XAxisX1 = 0;
-		XAxisX2 = XAxisLenTotal;
-		XAxisY = YAxisLenTotal - 20;
-
-		// y axis position
-		YAxisX2 = YAxisX1 = 50;
-		YAxisY1 = 0;
-		YAxisY2 = YAxisLenTotal;
+	private int treshold = -1;
+	{
 		this.setPreferredSize(new Dimension(XAxisLenTotal, YAxisLenTotal));
 	}
-
+	public GraphComp() {
+	}
+	public GraphComp(int[] pixelCount) throws IllegalArgumentException{
+		if (pixelCount.length != 256) {
+			throw new IllegalArgumentException(
+					"pixelCount must be of length 256");
+		}
+		countStep(pixelCount);
+		this.pixelCount = pixelCount;
+	}
 	@Override
 	protected void paintComponent(Graphics g) {
+
 		g.setFont(new Font("Dialog", Font.PLAIN, 8));
 
 		// y axis
-		g.drawLine(YAxisX1, YAxisY1, YAxisX2, YAxisY2);
+		g.drawLine(YAxisX, YAxisY1, YAxisX, YAxisY2);
 		// kreski
 		NumberFormat formatter = new DecimalFormat("0.##E0");
-		for (int i = XAxisY; i >= 0; i--) {
+		for (int i = 0; i <= YMax; i++) {
 			if (i % 10 == 0) {
-				g.drawLine(YAxisX1 - 5, XAxisY - i * pixPerUnit, YAxisX1, XAxisY - i
+				g.drawLine(YAxisX - 5, XAxisY - i * pixPerUnit, YAxisX, XAxisY - i
 						* pixPerUnit);
-				g.drawString(formatter.format(i * YAxisStep), YAxisX1 - 50,
+				g.drawString(formatter.format(i * YAxisStep), YAxisX - 50,
 						XAxisY - i * pixPerUnit + 3);
-				System.out.println(i * YAxisStep);
 			} else {
-				g.drawLine(YAxisX1 - 2, XAxisY - i * pixPerUnit, YAxisX1, XAxisY - i
+				g.drawLine(YAxisX - 2, XAxisY - i * pixPerUnit, YAxisX, XAxisY - i
 						* pixPerUnit);
 			}
 		}
 		// x axis
-		g.drawLine(XAxisX1, XAxisY, XAxisX2, XAxisY2);
+		g.drawLine(XAxisX1, XAxisY, XAxisX2, XAxisY);
 		// kreski
-		for (int i = 0; i <= 256; i++) {
+		for (int i = 0; i <= XMax; i++) {
 			if (i % 10 == 0) {
 				g.drawLine(XZeroXPos + i * pixPerUnit, XAxisY, XZeroXPos + i * pixPerUnit,
 						XAxisY + 5);
@@ -77,10 +85,15 @@ public class GraphComp extends JComponent {
 		// data
 		if (pixelCount != null) {
 			for (int i = 0; i < pixelCount.length; i++) {
-				int len = (int)Math.floor(pixelCount[i]/YAxisStep);
+				int len = (int)Math.floor((double)pixelCount[i]/YAxisStep);
 				g.setColor(Color.BLACK);
-				g.drawLine(XZeroXPos + i * pixPerUnit, XAxisY, XZeroXPos + i * pixPerUnit, XAxisY+len*pixPerUnit);
+				g.drawLine(XZeroXPos + i * pixPerUnit, XAxisY-len*pixPerUnit, XZeroXPos + i * pixPerUnit, XAxisY);
 			}
+		}
+		// treshold
+		if(treshold != -1){
+			g.setColor(Color.RED);
+			g.drawLine(XZeroXPos + treshold * pixPerUnit+1, 0, XZeroXPos + treshold * pixPerUnit+1, XAxisY);			
 		}
 	}
 
@@ -115,7 +128,14 @@ public class GraphComp extends JComponent {
 				max = pixelCount[i];
 			}
 		}
-		YAxisStep = max / YMax;
+		YAxisStep = (double)max / YMax;
 
 	}
+	public int getTreshold() {
+		return treshold;
+	}
+	public void setTreshold(int treshold) {
+		this.treshold = treshold;
+	}
+
 }
