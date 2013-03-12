78e3d1d4e640bef0e4826c90106719010d3ad893
bluk44 <bluk44@gmail.com>
2013-03-12 16:29:22 +0100

    dodanie reprezentacji tekstowej zdjęcia

--- Algorithms/src/components/ImageComponent.java
+++ Algorithms/src/components/ImageComponent.java
@@ -45,6 +45,21 @@ public class ImageComponent extends JComponent {
 		g.drawImage(image, 0, 0, null);
 	}
 	
+	public void print(){
+		int imgW = image.getWidth(), imgH = image.getHeight();
+		for(int b = 0;b < image.getRaster().getNumBands(); b++){
+			
+			int[] samples = new int[imgH*imgW];
+			image.getRaster().getSamples(0, 0, imgW, imgH, b, samples);
+			System.out.println("band "+b+": ");
+			for (int i = 0; i < imgH; i++) {
+				for (int j = 0; j < imgW; j++) {
+					System.out.format("%04d ",samples[i*imgW+j]);
+				}
+				System.out.println();
+			}
+		}
+	}
 	private void fitToImage(){
 		if(image == null){
 			return;
